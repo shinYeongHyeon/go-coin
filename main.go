1fa096b24b9c928abd4e500f3ed12e944e3db110
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-06-22 22:31:14 +0900

    First Genesis Block

--- main.go
+++ main.go
@@ -1,7 +1,21 @@
 package main
 
-import "fmt"
+import (
+	"crypto/sha256"
+	"fmt"
+)
+
+type block struct {
+	data string
+	hash string
+	prevHash string
+}
 
 func main() {
-	fmt.Println("Start!")
+	genesisBlock := block {
+		data: "Genesis Block",
+		hash: "",
+		prevHash: "",
+	}
+	genesisBlock.hash = fmt.Sprintf("%x", sha256.Sum256([]byte(genesisBlock.data + genesisBlock.prevHash)))
 }
