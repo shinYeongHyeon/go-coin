25257695f4b998b86c5da27e20dfd5a88a9de032
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-06-22 23:20:08 +0900

    blockain dir and refactor

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -1,22 +1,63 @@
 package blockchain
 
+import (
+	"crypto/sha256"
+	"fmt"
+	"sync"
+)
+
 type block struct {
-	data string
-	hash string
-	prevHash string
+	Data     string
+	Hash     string
+	PrevHash string
 }
 
 type blockchain struct {
-	blocks []block
+	blocks []*block
 }
 
 var b *blockchain // 변수의 인스턴스를 직접 공유하지 않음 -> Singleton Pattern
+var once sync.Once
+
+func (b *block) calculateHash() {
+	b.Hash = fmt.Sprintf("%x", sha256.Sum256([]byte(b.Data+ b.PrevHash)))
+}
+
+func getLastHash() string {
+	totalBlocks := len(GetBlockChain().blocks)
+	if totalBlocks == 0 {
+		return ""
+	}
+
+	return GetBlockChain().blocks[totalBlocks - 1].Hash
+}
+
+func createBlock(data string) *block {
+	newBlock := block {data, "", getLastHash()}
+	newBlock.calculateHash()
+
+	return &newBlock
+}
+
+// AddBlock Add block to chain
+func (b *blockchain) AddBlock(data string) {
+	b.blocks = append(b.blocks, createBlock(data))
+}
 
 // GetBlockChain Getter BlockChain Instance
 func GetBlockChain() *blockchain {
 	if b == nil {
-		b = &blockchain {}
+		// 단 한번만 실행하도록 도와주는 sync 라이브러리
+		once.Do(func() {
+			b = &blockchain {}
+			b.AddBlock("Genesis Block")
+		})
 	}
 
 	return b
+}
+
+// AllBlocks Get All Blocks
+func (b *blockchain) AllBlocks() []*block {
+	return b.blocks
 }
--- main.go
+++ main.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/shinYeongHyeon/go-coin/blockchain"
+import (
+	"fmt"
+	"github.com/shinYeongHyeon/go-coin/blockchain"
+)
 
 func main() {
 	chain := blockchain.GetBlockChain()
+	chain.AddBlock("Second Block")
+	chain.AddBlock("Third Blocks")
+	for _, block := range chain.AllBlocks() {
+		fmt.Printf("Data: %s\n", block.Data)
+		fmt.Printf("Hash: %s\n", block.Hash)
+		fmt.Printf("Previous Hash: %s\n", block.PrevHash)
+		fmt.Println()
+	}
 }
