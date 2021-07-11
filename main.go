6869cc72404e03f52614fd357cc4dfc46f13149f
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-07-11 23:16:19 +0900

    RestAPI Setup

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "github.com/shinYeongHyeon/go-coin/explorer"
+import (
+	"encoding/json"
+	"fmt"
+	"github.com/shinYeongHyeon/go-coin/utils"
+	"log"
+	"net/http"
+)
+
+const port string = ":4000"
+
+type URLDescription struct {
+	URL string
+	Method string
+	Description string
+}
+
+func documentation(rw http.ResponseWriter, r *http.Request) {
+	data := []URLDescription {
+		{
+			URL: 		 "/",
+			Method: 	 "GET",
+			Description: "See Documentation",
+		},
+	}
+	bytes, err := json.Marshal(data)
+	utils.HandleError(err)
+	fmt.Printf("%s", bytes)
+
+}
 
 func main() {
-	explorer.Start()
+	http.HandleFunc("/", documentation)
+	fmt.Printf("Listening on http://localhost%s", port)
+	log.Fatal(http.ListenAndServe(port, nil))
 }
