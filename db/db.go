ae64db1c28d9bbb1413a0544ff22184ef6b68767
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-08-12 00:44:33 +0900

    Init DB (w/ Bolt)

--- db/db.go
+++ db/db.go
@@ -1,1 +1,18 @@
 package db
+
+import (
+	"github.com/boltdb/bolt"
+	"github.com/shinYeongHyeon/go-coin/utils"
+)
+
+var db *bolt.DB
+
+func DB() *bolt.DB {
+	if db == nil {
+		dbPoint, err := bolt.Open("blockchain.db", 0600, nil)
+		utils.HandleError(err)
+		db = dbPoint
+	}
+
+	return db
+}
