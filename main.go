2222947f22c26afb16a166e658f1556a1dbdc57a
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-08-17 00:07:45 +0900

    PoW Proof Of Concept

--- main.go
+++ main.go
@@ -1,11 +1,24 @@
 package main
 
 import (
-	"github.com/shinYeongHyeon/go-coin/cli"
-	"github.com/shinYeongHyeon/go-coin/db"
+	"crypto/sha256"
+	"fmt"
+	"strings"
 )
 
 func main() {
-	defer db.Close()
-	cli.Start()
+	difficulty := 2
+	target := strings.Repeat("0", difficulty)
+	nonce := 1
+	for {
+		hash := fmt.Sprintf("%x", sha256.Sum256([]byte("hello" + fmt.Sprint(nonce))))
+		fmt.Printf("Hash : %s\nTarget: %s\nNonce: %d\n\n", hash, target, nonce)
+		if strings.HasPrefix(hash, target) {
+			return
+		} else {
+			nonce++
+		}
+	}
+	// defer db.Close()
+	// cli.Start()
 }
