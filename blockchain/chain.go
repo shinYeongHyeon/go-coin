cf9c27cf8e2d84f3af80f3dd51f1eb02444fb775
ShinYeongHyeon <den.shin.dev@gmail.com>
2021-08-28 15:51:44 +0900

    Recalculate Difficulty Part1 / ~5.52

--- blockchain/chain.go
+++ blockchain/chain.go
@@ -8,8 +8,9 @@ import (
 )
 
 const (
-	defaultDifficulty int = 2
+	defaultDifficulty  int = 2
 	difficultyInterval int = 5
+	blockInterval 	   int = 2
 )
 
 type blockchain struct {
@@ -52,12 +53,26 @@ func (b *blockchain) Blocks() []*Block {
 	return blocks
 }
 
+func (b *blockchain) recalculateDifficulty() int {
+	allBlocks := b.Blocks()
+	newestBlock := allBlocks[0]
+	lastRecalculatedBlock := allBlocks[difficultyInterval - 1]
+	actualTime := (newestBlock.Timestamp/60) - (lastRecalculatedBlock.Timestamp/60)
+	expectedTime := difficultyInterval * blockInterval
+	if actualTime < expectedTime {
+		return b.CurrentDifficulty + 1
+	} else if actualTime > expectedTime {
+		return b.CurrentDifficulty - 1
+	}
+
+	return b.CurrentDifficulty
+}
+
 func (b *blockchain) difficulty() int {
 	if b.Height == 0 {
 		return defaultDifficulty
 	} else if b.Height & difficultyInterval == 0 {
-		// recalculate the difficulty
-		return 0
+		return b.recalculateDifficulty()
 	} else {
 		return b.CurrentDifficulty
 	}
