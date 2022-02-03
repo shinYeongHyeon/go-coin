ef7986778b81a61145f6e240b93d2ade95220804
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-02-03 22:36:12 +0900

    makeTx

--- blockchain/transactions.go
+++ blockchain/transactions.go
@@ -61,7 +61,47 @@ func makeTx(from, to string, amount int) (*Tx, error) {
 		return nil, errors.New("not enough money")
 	}
 
-	// from -> to
+	var txIns []*TxIn
+	var txOuts []*TxOut
+	var total = 0
+	oldTxOuts := Blockchain().TxOutsByAddress(from)
+	for _, txOut := range oldTxOuts {
+		if total > amount {
+			break
+		}
+
+		txIn := &TxIn{
+			txOut.Owner,
+			txOut.Amount,
+		}
+		txIns = append(txIns, txIn)
+		total += txIn.Amount
+	}
+
+	change := total - amount
+
+	if change != 0 {
+		changeTxOut := &TxOut{
+			from,
+			change,
+		}
+		txOuts = append(txOuts, changeTxOut)
+	}
+
+	txOut := &TxOut{
+		to,
+		amount,
+	}
+	txOuts = append(txOuts, txOut)
+	tx := &Tx{
+		Id:        "",
+		Timestamp: int(time.Now().Unix()),
+		TxIns:     txIns,
+		TxOuts:    txOuts,
+	}
+	tx.getId()
+
+	return tx, nil
 }
 
 func (m *mempool) AddTx(to string, amount int) error {
